15335678c6e2fd6695887779c60e21d707bba3dc
massivegoose <ombrewang2005@gmail.com>
2020-08-21 01:43:56 +0000

    is_prime update

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,6 @@
 def is_prime(n): return all(map(lambda a: n / a % 1 != 0, range(2, n)))
 
-print("testing is_prime on input 59...")
+print("testing is_prime on input p59...")
 assert is_prime(59)
 print("PASSED")
 
