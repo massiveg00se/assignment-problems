39ce5b20032aac4ad27e9adacf4c6b2fe4fcd9a1
massive goose <ombrewang2005@gmail.com>
2020-08-31 05:12:39 +0000

    problem 2-1

---
+++ union_intersection.py
@@ -0,0 +1,22 @@
+def intersection(a, b):
+  result = []
+  for i in a:
+    if i in b:
+      result.append(i)
+  return result
+
+def union(a, b):
+  result = []
+  for i in range(len(a)):
+    result.append(a[i])
+    if i < len(b) and b[i] not in a:
+      result.append(b[i])
+  return result
+
+print("testing intersection() on inputs [1,2,'a','b'], [2,3,'a']...")
+assert intersection([1,2,'a','b'], [2,3,'a']) == [2,'a'], "There is something wrong."
+print("PASSED")
+
+print("testing union() on inputs [1,2,'a','b'], [2,3,'a']...")
+assert union([1,2,'a','b'], [2,3,'a']) == [1,2,3,'a','b'], "There is something wrong."
+print("PASSED")
